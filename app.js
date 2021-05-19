a41ca80b8d44eca939b782ecb018e626822989e7
jarfile <adnanmukhtar@gmail.com>
2021-05-19 00:37:10 -0400

    cosmetics

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/ataglance', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

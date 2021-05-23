c7738b98bbe6997b5284aad9a76affb2b050e088
jarfile <adnanmukhtar@gmail.com>
2021-05-22 23:37:07 -0400

    added search

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,55 @@ const router = express.Router();
 const toc = Data.get('toc');
 const metadata = Data.get('metadata');
 const quran = Data.get('quran');
+const squran = Data.get('quran.clean');
 const equran = Data.get('en.saheeh');
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    toc: toc,
-    quran: quran,
-    md: metadata
-  });
+  if (req.query.q) {
+    var results = [];
+    if (req.query.q.match(/[a-z]/i)) {
+      var q = new RegExp('(' + req.query.q + ')', 'ig');
+      for (var i = 0; i < equran.length; i++) {
+        for (var j = 0; j < equran[i].ayas.length; j++) {
+          if (equran[i].ayas[j].text.toLowerCase().match(q))
+            results.push({
+              "sura": i + 1,
+              "aya": j + 1,
+              "text": quran[i].ayas[j].text,
+              "trans": equran[i].ayas[j].text.replace(q, '<em>$1</em>')
+            });
+          if (results.length > 1000)
+            break;
+        }
+      }
+    } else {
+      for (var i = 0; i < quran.length; i++) {
+        var q = new RegExp('(' + req.query.q + ')', 'g');
+        for (var j = 0; j < quran[i].ayas.length; j++) {
+          if (squran[i].ayas[j].text.match(q))
+            results.push({
+              "sura": i + 1,
+              "aya": j + 1,
+              "text": squran[i].ayas[j].text.replace(q, '<em>$1</em>'),
+              "trans": equran[i].ayas[j].text
+            });
+          if (results.length > 1000)
+            break;
+        }
+      }
+    }
+    res.render('search', {
+      q: req.query.q,
+      results: results,
+      quran: quran,
+      equran: quran
+    });
+  } else
+    res.render('index', {
+      toc: toc,
+      quran: quran,
+      md: metadata
+    });
 });
 
 router.get('/r/:q', function (req, res, next) {
